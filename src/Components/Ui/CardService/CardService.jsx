6dc0584da876cc53aa018b8a1ab9d9f7f392764c
camilo3312 <ccvelasquez531@misena.edu.co>
2022-03-10 01:42:39 -0500

    camilo_dev se añade nueva pagina index

--- src/Components/Ui/CardService/CardService.jsx
+++ src/Components/Ui/CardService/CardService.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { ReactComponent as IconLocation } from "Assets/Icons/IconLocation.svg"
+
 import './CardService.css'
 
 export const CardService = ({info}) => {
@@ -7,12 +9,19 @@ export const CardService = ({info}) => {
         <div className='card_service'>
             <header className='header_card_service'>
                 <div className='price_header_card'>
-                    <p className='price_card'>$ {price} por hora</p>              
+                    <div className="information_price_service">
+                        <p>$</p>
+                        <p>{price}</p>
+                        <p>por hora</p>                             
+                    </div>
                 </div>
                 <img className='image_header_card' src={image} alt="" />
             </header>
             <body className='body_card_service'>
-                <p className='location_body_card'>{city} • {departament}</p>
+                <div className="location_card_service">
+                    <IconLocation className='icon_location_card_service'/>
+                    <p className='location_body_card'>{city} • {departament}</p>
+                </div>
                 <p className='title_body_card'>{title} </p>
             </body>
         </div>
