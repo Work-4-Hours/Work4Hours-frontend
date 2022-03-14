8889cfbc4942191b996e5c78a58637577f5093f2
MajoS7 <85207019+MajoS7@users.noreply.github.com>
2022-03-14 00:19:30 -0500

    majo_dev - Diseño del menu del admin terminado

--- src/Components/Layout/MenuAdmin/MenuAdmin.jsx
+++ src/Components/Layout/MenuAdmin/MenuAdmin.jsx
@@ -1,5 +1,9 @@
+import { Button } from 'Components/Ui/Button/Button';
 import { TitleAdmin } from 'Components/Ui/TitleAdmin/TitleAdmin';
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 import './MenuAdmin.css';
 
@@ -7,6 +11,13 @@ export const MenuAdmin = ({nameAdmin}) => {
   return (
     <div className='menu_admin'>
       <TitleAdmin typeAdmin={nameAdmin}/>
+      <div className='btns_menu_admin'>
+        <Link to="/AdminUsers"><Button className="button btn_with_admin" value={"Usuarios"}/></Link>
+        <Link to="/AdminServices"><Button className="button btn_change_color_gray btn_with_admin" value={"Servicios"}/></Link>
+      </div>
+      <div className='btn_log_out_admin'>
+      <Link to="/"><Button className="button btn_change_color_gray btn_with_admin" value={"Cerrar Sesión"}/></Link>
+      </div>
     </div> 
   )
 }
