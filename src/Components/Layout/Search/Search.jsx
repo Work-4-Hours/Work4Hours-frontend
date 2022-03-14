7050c30e007ac2fefc86fa47e68853a7e7c7e45b
MajoS7 <85207019+MajoS7@users.noreply.github.com>
2022-03-13 23:30:50 -0500

    majo_dev Diseño del boton filtro

--- src/Components/Layout/Search/Search.jsx
+++ src/Components/Layout/Search/Search.jsx
@@ -1,12 +1,15 @@
+import { Button } from 'Components/Ui/Button/Button';
 import { SearchBox } from 'Components/Ui/SearchBox/SearchBox';
 import React from 'react';
 import './Search.css';
 
 
+
 export const Search = ({nameSearch}) => {
   return (
-    <div>
+    <div className='search_filter_admin_users'>
       <SearchBox typeSearch={nameSearch}/>
+      <Button className="btn_search-filter_admin_users button" value={"Filtro"}/>
     </div>
   )
 }
--- src/Components/Ui/Button/Button.jsx
+++ src/Components/Ui/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Button.css'
 
-export const Button = ({value = 'boton', ...props }) => {
+export const Button = ({value, ...props }) => {
     return (
         <button className='button' {...props} >{value}</button>
     )
