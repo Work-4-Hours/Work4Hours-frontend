250083f596ed88ea3d2a0e85faf35194bf2df076
Majos7 <majos2764@gmail.com>
2022-03-15 17:59:37 -0500

    majo_dev - Modificacion del estado del usuario

--- src/Components/Ui/SelectAdmin/SelectAdmin.jsx
+++ src/Components/Ui/SelectAdmin/SelectAdmin.jsx
@@ -7,30 +7,27 @@ import './SelectAdmin.css'
 export const SelectAdmin = () => {
 
   const [stateUser, setStateUser]=useState("Habilitado");
-  const [option, setOption] = useState(false);
-  
-  const changeUserStatus=()=>{
-    
-  }
-   
-  
+  const [isOpen, setIsOpen] = useState(false)
 
+  const changeStateUsers=(event)=>{
+    setStateUser(event.target.textContent)
+    setIsOpen(!isOpen)
+  }
+
+  // const closePopUp=()=>{
+  //   setIsOpen(true)
+  // }
+  
   return (
-    
     <div className='cb_state_user' >
-      <p className='op_state_user_initial' onClick={changeUserStatus() }>{stateUser}</p>
-       <div className='content_options'>
+      <p className='op_state_user_initial green' onClick={event=>setIsOpen(!isOpen)}>{stateUser}</p>
+      <PopUp className='content_options' isOpen={isOpen} >
         <h5 className='spacing'>Estados del usuario</h5>
-        <div className='option_spacing'>
-          <p className='op_state_user spacing'>Habilitado</p><p></p>
-        </div>
-        <div className='option_spacing'>
-          <p className='op_state_user spacing'>Inhabilitado</p><p></p>
-        </div>
-        <div className='option_spacing'>
-          <p className='op_state_user spacing'>Suspendido 3 dias</p><p></p>
-        </div>
-      </div> 
+        <p className='op_state_user spacing green' onClick={event=>changeStateUsers(event)}>ㅤHabilitado</p>
+        <p className='op_state_user spacing yellow' onClick={event=>changeStateUsers(event)}>ㅤSuspendido 3 dias</p>
+        <p className='op_state_user spacing red' onClick={event=>changeStateUsers(event)}>ㅤInhabilitado</p>
+      </PopUp>
+
     </div>
   )
 }
