676c14f5571cce9459a38dc9a4f2a4d7121171b7
Majos7 <majos2764@gmail.com>
2022-03-17 13:33:45 -0500

    majo_dev - Se elimino el caracter de espacio para evitar futuros errores

--- src/Components/Ui/StatusUsersAdmin/StatusUsersAdmin.jsx
+++ src/Components/Ui/StatusUsersAdmin/StatusUsersAdmin.jsx
@@ -13,18 +13,15 @@ export const StatusUsersAdmin = () => {
     setStateColor(event.target.classList[2])
   }
 
-  // const closePopUp=()=>{
-  //   setIsOpen(true)
-  // }
   
   return (
     <div className='position_relative' >
       <p className={'op_state_user_initial ' + stateColor}  onClick={event=>setIsOpen(!isOpen)}>{stateUser}</p>
       <PopUp className='content_options' isOpen={isOpen} >
         <h5 className='spacing'>Estados del usuario</h5>
-        <p className='op_state_user spacing green' onClick={event=>changeStateUsers(event)}>ㅤHabilitado</p>
-        <p className='op_state_user spacing yellow' onClick={event=>changeStateUsers(event)}>ㅤSuspendido 3 dias</p>
-        <p className='op_state_user spacing red' onClick={event=>changeStateUsers(event)}>ㅤInhabilitado</p>
+        <p className='op_state_user spacing green' onClick={event=>changeStateUsers(event)}>Habilitado</p>
+        <p className='op_state_user spacing yellow' onClick={event=>changeStateUsers(event)}>Suspendido 3 dias</p>
+        <p className='op_state_user spacing red' onClick={event=>changeStateUsers(event)}>Inhabilitado</p>
       </PopUp>
     </div>
   )
