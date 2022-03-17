395041fef31ef3511618a6817e303b728fcd03b0
Mari293 <maricruzg020903@gmail.com>
2022-03-17 13:18:55 -0500

    mariana_dev - Cambio de color en el estado de usuario

--- src/Components/Ui/StatusUsersAdmin/StatusUsersAdmin.jsx
+++ src/Components/Ui/StatusUsersAdmin/StatusUsersAdmin.jsx
@@ -2,13 +2,10 @@ import { PopUp } from 'Components/StyleComponets/PopUp';
 import {React,useState} from 'react'
 import './StatusUsersAdmin.css'
 
-
-
 export const StatusUsersAdmin = () => {
-
   const [stateUser, setStateUser]=useState("Habilitado");
   const [isOpen, setIsOpen] = useState(false);
-  const [stateColor, setStateColor]=useState("")
+  const [stateColor, setStateColor]=useState('green');
 
   const changeStateUsers=(event)=>{
     setStateUser(event.target.textContent)
@@ -22,7 +19,7 @@ export const StatusUsersAdmin = () => {
   
   return (
     <div className='position_relative' >
-      <p className='op_state_user_initial green'  onClick={event=>setIsOpen(!isOpen)}>{stateUser}</p>
+      <p className={'op_state_user_initial ' + stateColor}  onClick={event=>setIsOpen(!isOpen)}>{stateUser}</p>
       <PopUp className='content_options' isOpen={isOpen} >
         <h5 className='spacing'>Estados del usuario</h5>
         <p className='op_state_user spacing green' onClick={event=>changeStateUsers(event)}>ㅤHabilitado</p>
