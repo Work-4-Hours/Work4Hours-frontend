17e6be8eec8343032b409b3b6fdf6bc596b8350f
MajoS7 <85207019+MajoS7@users.noreply.github.com>
2022-03-16 23:11:08 -0500

    majo_dev - Diseño del filtro de busqueda sin los checkbox completos

--- src/Components/Ui/CheckBoxAdmin/CheckBoxAdmin.jsx
+++ src/Components/Ui/CheckBoxAdmin/CheckBoxAdmin.jsx
@@ -3,9 +3,11 @@ import './CheckBoxAdmin.css'
 
 export const CheckBoxAdmin = () => {
   return (
-    <label className='cb_confirm_changes text_center'>
-        <input type="checkbox"/>
-        <span></span>
-    </label>
+    <div className='text_center'>
+      <label className='lb_confirm_changes'>
+          <input type="checkbox" className='cb_confirm_changes'/>
+          <span className='span_confirm_changes'></span>
+      </label>
+    </div>
   )
 }
--- src/Components/Ui/FilterAdmin/FilterAdmin.jsx
+++ src/Components/Ui/FilterAdmin/FilterAdmin.jsx
@@ -2,18 +2,19 @@ import { PopUp } from 'Components/StyleComponets/PopUp';
 import {React,useState} from 'react'
 import { Button } from '../Button/Button';
 import { CheckBoxAdmin } from '../CheckBoxAdmin/CheckBoxAdmin';
+import './FilterAdmin.css'
 
 
 
 export const FilterAdmin = () => {
     const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className='position_relative btn_search-filter_admin_users' >
-        <Button className=" button" value={"Filtro"} onClick={event=>setIsOpen(!isOpen)}/>
-      <PopUp className='content_options' isOpen={isOpen} > 
+    <div className='position_relative content_filter' >
+        <Button className=" button btn_search_filter_admin_users" value={"Filtro"} onClick={event=>setIsOpen(!isOpen)}/>
+      <PopUp className='content_options filter_options' isOpen={isOpen} > 
         <p className='spacing'>Tipos de suspención</p>
         <div className="typ_report spacing">
-          <p>Reportes</p><CheckBoxAdmin/>
+          <p>Reportes</p><CheckBoxAdmin className="checkbox_filter"/>
         </div>
         <div className="typ_report spacing">
           <p>Correo</p><CheckBoxAdmin/>
