a9c581709e4d3f4063781a9a7213fe0a2d8c8b3d
Majos7 <majos2764@gmail.com>
2022-03-17 14:36:39 -0500

    majo_dev - Diseño del titulo del popup

--- src/Components/Ui/PopupTitleAdmin/PopupTitleAdmin.jsx
+++ src/Components/Ui/PopupTitleAdmin/PopupTitleAdmin.jsx
@@ -4,9 +4,9 @@ import './PopupTitleAdmin.css'
 
 export const PopupTitleAdmin = () => {
   return (
-    <div>
+    <div className='popup_title_admin'>
         <GoAlert className='icon_alert_admin'/>
-        <h2>Esta seguro de querer actualizar el estado de: </h2>
+        <h3>Esta seguro de querer actualizar el estado de: </h3>
     </div>
   )
 }
