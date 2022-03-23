dc4531aa6e4292a66992366291704bf101dce5cf
Mari293 <maricruzg020903@gmail.com>
2022-03-23 17:53:37 -0500

    mariana_dev - Se mandan estilos por parametro a DashboardHeader

--- src/Components/Layout/DashboardHeader/DashboardHeader.jsx
+++ src/Components/Layout/DashboardHeader/DashboardHeader.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './DashboardHeader.css';
 
-export const DashboardHeader = ({header1,header2,header3,header4,header5,header6,header7,propsReport}) => {
+export const DashboardHeader = ({space1,space2,space3,space4,space5,header1,header2,header3,header4,header5,header6,header7,propsReport}) => {
   return (
     <div className='dashboard_header'>
-      <p className='fieldSize3'>{header1}</p>
-      <p className='fieldSize21'>{header2}</p>
-      <p className='fieldSize21'>{header3}</p>
-      <p className='fieldSize22'>{header4}</p>
-      <p className={'fieldSize5 '+ propsReport}>{header5}</p>
-      <p className='fieldSize8'>{header6}</p>
+      <p className={space1}>{header1}</p>
+      <p className={space4}>{header2}</p>
+      <p className={space4}>{header3}</p>
+      <p className={space5}>{header4}</p>
+      <p className={space2 + propsReport}>{header5}</p>
+      <p className={space3}>{header6}</p>
       <p className='fieldSize8'>{header7}</p>
     </div>
   )
--- src/Modules/Admin/Users/Users.jsx
+++ src/Modules/Admin/Users/Users.jsx
@@ -14,7 +14,7 @@ export const Users = () => {
       <MenuAdmin nameAdmin={"Usuarios"} btnActive={"button btn_with_admin"} btnInactive={"button btn_change_color_gray btn_with_admin"}/>
       <div className='manager_control'>
         <Search nameSearch={"Buscar Usuarios"}/>
-        <DashboardHeader header1={"Perfil"} header2={"Apellidos"} header3={"Nombres"} header4={"Correo"} header5={"Reportes"} header6={"Estado Usuario"} header7={"Conf. cambios"} />
+        <DashboardHeader space1={'fieldSize3 '} space2={'fieldSize5 '} space3={'fieldSize8 '} space4={'fieldSize21 '} space5={'fieldSize22 '} header1={"Perfil"} header2={"Apellidos"} header3={"Nombres"} header4={"Correo"} header5={"Reportes"} header6={"Estado Usuario"} header7={"Conf. cambios"} />
         <Dashboard content={<UserInfo/> }/>
         <PopupConfirmChanges/>
       </div>
