c4e9d120280c39db813f8f28a9819a0b3dfa0914
Mari293 <maricruzg020903@gmail.com>
2022-03-28 15:32:20 -0500

    mariana_dev - Se termina de dar estilos en el popup de descripcion del servicio en administrador

--- src/Components/Ui/DescriptionServiceAdmin/DescriptionServiceAdmin.jsx
+++ src/Components/Ui/DescriptionServiceAdmin/DescriptionServiceAdmin.jsx
@@ -1,17 +1,17 @@
 import React, {useState} from 'react'
-import'./MessageUserAdmin.css'
+import'./DescriptionServiceAdmin.css'
 
 import { PopUpOptions } from 'Components/StyleComponets/PopupOptions';
 
-export const MessageUserAdmin = () => {
+export const DescriptionServiceAdmin = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [display, setDisplay]=useState('none');
     
   return (
-    <div className='fieldSize8'>
-      <p className='position_relative' onClick={()=>{setIsOpen(!isOpen); setDisplay('block')}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, veniam sit exercitationem minus dolorum, aspernatur, ullam voluptate voluptates labore nam minima quae eos neque ipsum. Beatae ad eos similique veniam.</p>
+    <div className='fieldSize21 position_relative'>
+      <p className="ellipsis count_reports" onClick={()=>{setIsOpen(!isOpen); setDisplay('block')}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, veniam sit exercitationem minus dolorum, aspernatur, ullam voluptate voluptates labore nam minima quae eos neque ipsum. Beatae ad eos similique veniam.</p>
       <PopUpOptions className='content_description' isOpen={isOpen} visibilidad={display}>
-        <h5 className='spacing title_popup_description'>Mensaje</h5>
+        <h5 className='spacing title_popup_description'>Descripción</h5>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, veniam sit exercitationem minus dolorum, aspernatur, ullam voluptate voluptates labore nam minima quae eos neque ipsum. Beatae ad eos similique veniam.</p>
       </PopUpOptions>
     </div>
--- src/Components/Ui/ServiceInfo/ServiceInfo.jsx
+++ src/Components/Ui/ServiceInfo/ServiceInfo.jsx
@@ -4,7 +4,7 @@ import './ServiceInfo.css';
 import { CheckBoxAdmin } from '../CheckBoxAdmin/CheckBoxAdmin';
 import { InfoReportAdmin } from '../InfoReportAdmin/InfoReportAdmin';
 import { StatusUsersAdmin } from '../StatusUsersAdmin/StatusUsersAdmin';
-import { MessageUserAdmin } from '../MessageUserAdmin/MessageUserAdmin';
+import { DescriptionServiceAdmin } from '../DescriptionServiceAdmin/DescriptionServiceAdmin';
 
 export const ServiceInfo = () => {
   return (
@@ -14,7 +14,7 @@ export const ServiceInfo = () => {
         <img className='admin_user_photo center_img' src="https://cdn.pixabay.com/photo/2021/09/12/08/49/headset-6617715_960_720.png" alt="user_photo" />
         <p className='ellipsis'>Fantasmita </p>
       </div>
-      <p className='ellipsis fieldSize21'><MessageUserAdmin/></p>
+      <DescriptionServiceAdmin/>
       <p className='ellipsis fieldSize21'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, veniam sit exercitationem minus dolorum, aspernatur, ullam voluptate voluptates labore nam minima quae eos neque ipsum. Beatae ad eos similique veniam.</p>
       <InfoReportAdmin/>
       <StatusUsersAdmin/>
