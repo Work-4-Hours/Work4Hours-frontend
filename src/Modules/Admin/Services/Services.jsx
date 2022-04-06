3665d2d8fc868cfa248f4ce6616968e0d5c9f121
Mari293 <maricruzg020903@gmail.com>
2022-04-06 17:00:34 -0500

    mariana_dev - Se cambia nombre de propiedad para que no distorione otros componentes

--- src/Modules/Admin/Services/Services.jsx
+++ src/Modules/Admin/Services/Services.jsx
@@ -17,7 +17,7 @@ export const Services = () => {
       <MenuAdmin nameAdmin={"Servicios"} btnActive={"button btn_change_color_gray btn_with_admin"} btnInactive={"button btn_with_admin"}/>
       <div className='manager_control'>
         <Search nameSearch={"Buscar Servicios"} filter={<FilterServiceAdmin/>}/>
-        <DashboardHeader space1={'fieldSize15 '} space2={'fieldSize15 '} space3={'fieldSize21 '} space4={'fieldSize21 '} space5={'fieldSize5 '} space6={'fieldSize8 '} space7={'fieldSize8 '} header1={"Servicio"} header2={"Usuario"} header3={"Descripción"} header4={"Apelación"} header5={"Reportes"} header6={"Estado Usuario"} header7={"Seleccionar"} propsReport={"report"}/>
+        <DashboardHeader space1={'fieldSize15 '} space2={'fieldSize15 '} space3={'fieldSize21 '} space4={'fieldSize21 '} space5={'fieldSize5 '} space6={'fieldSize8 '} space7={'fieldSize8 '} header1={"Servicio"} header2={"Usuario"} header3={"Descripción"} header4={"Apelación"} header5={"Reportes"} header6={"Estado Usuario"} header7={"Seleccionar"} propsReport={"reportColor"}/>
         <Dashboard content={<ServiceInfo/>}/>
         <PopupConfirmChanges nameTitle={"Esta seguro de querer eliminar: "} valueButton={"Eliminar"} objectContent={<ObjectDelete/>} styleObjects={"popup_confirm_changes_content_objects_services"}/>
       </div>
