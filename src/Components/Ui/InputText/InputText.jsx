d39a58b0a21cfed15c0340b14c672b0de7f316c9
camilo3312 <ccvelasquez531@misena.edu.co>
2022-04-18 17:16:12 -0500

    camilo_dev resolucion de problemas

--- src/Components/Ui/InputText/InputText.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import './InputText.css'
-
-export const InputText = ({...props}) => {
-    return (
-       <input className='input_text' {...props} />
-    )
-}
--- src/Components/Ui/InputTextarea/InputTextarea.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './InputTextarea.css'
-
-export const InputTextarea = ({...props}) => {
-    return (
-        <textarea {...props} className='input_textarea' cols="30" rows="10"></textarea>
-    )
-}
