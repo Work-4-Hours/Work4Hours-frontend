e891bc9a4c58fef5f7bea02127c49336a3f2ef47
Mari293 <maricruzg020903@gmail.com>
2022-04-18 17:23:03 -0500

    mariana_dev - Se cambia la manera de mostrar el estado de un usuario en admin

--- src/Components/Ui/StatusUsersAdmin/StatusUsersAdmin.jsx
+++ src/Components/Ui/StatusUsersAdmin/StatusUsersAdmin.jsx
@@ -6,24 +6,23 @@ export const StatusUsersAdmin = () => {
   const [stateUser, setStateUser]=useState("Habilitado");
   const [isOpen, setIsOpen] = useState(false);
   const [stateColor, setStateColor]=useState('green');
-  const [display, setDisplay]=useState('none');
 
   const changeStateUsers=(event)=>{
     setStateUser(event.target.textContent)
     setIsOpen(!isOpen)
-    setStateColor(event.target.classList[2])
+    setStateColor(event.target.id)
   }
 
   return (
     <div className='position_relative fieldSize8'>
-      <p className={'op_state_user ' + stateColor} onClick={()=>{setIsOpen(!isOpen); setDisplay('block')}}>{stateUser}</p>
+      <p className={'op_text_state_users ' + stateColor} onClick={()=>{setIsOpen(!isOpen)}}>{stateUser}</p>
       <PopUp isOpen={isOpen}>
-        <div className='overlay overlay_options' onClick={()=>{setIsOpen(!isOpen); setDisplay('block')}}></div>
+        <div className='overlay overlay_options' onClick={()=>{setIsOpen(!isOpen)}}></div>
         <div className='content_options'>
           <h5 className='spacing'>Estados del usuario</h5>
-          <p className='op_state_user spacing green field105px' onClick={event=>changeStateUsers(event)}>Habilitado</p>
-          <p className='op_state_user spacing yellow field160px' onClick={event=>changeStateUsers(event)}>Suspendido 3 dias</p>
-          <p className='op_state_user spacing red field116px' onClick={event=>changeStateUsers(event)}>Inhabilitado</p>
+          <p className='op_state_user spacing state_1 field105px' id="green" onClick={event=>changeStateUsers(event)}>Habilitado</p>
+          <p className='op_state_user spacing state_2 field160px' id="yellow" onClick={event=>changeStateUsers(event)}>Suspendido 3 dias</p>
+          <p className='op_state_user spacing state_3 field116px' id="red" onClick={event=>changeStateUsers(event)}>Inhabilitado</p>
         </div>
       </PopUp>
     </div>
