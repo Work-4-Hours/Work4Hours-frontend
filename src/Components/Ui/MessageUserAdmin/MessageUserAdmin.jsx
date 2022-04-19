6e97f8ce362d2be3a73680617c77cd59182f0f31
mari293 <maricruzg020903@gmail.com>
2022-04-18 23:06:23 -0500

    mariana_dev - Se cambian el tamaño de los espacios en dashboard de servicios en admin

--- src/Components/Ui/MessageUserAdmin/MessageUserAdmin.jsx
+++ src/Components/Ui/MessageUserAdmin/MessageUserAdmin.jsx
@@ -6,7 +6,7 @@ export const MessageUserAdmin = () => {
     const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className='fieldSize21 position_relative'>
+    <div className='fieldSize15 position_relative'>
       <p className="ellipsis pointer_userSelect_none" onClick={()=>{setIsOpen(!isOpen)}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, veniam sit exercitationem minus dolorum, aspernatur, ullam voluptate voluptates labore nam minima quae eos neque ipsum. Beatae ad eos similique veniam.</p>
       <PopUp isOpen={isOpen}>
         <div className='overlay overlay_options' onClick={()=>{setIsOpen(!isOpen)}}></div>
