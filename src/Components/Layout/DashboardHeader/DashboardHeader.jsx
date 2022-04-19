565dea2f68262bb7326524de421b8651946b6861
Mari293 <maricruzg020903@gmail.com>
2022-04-19 12:25:34 -0500

    mariana_dev - Se organiza que quede de manera centrada un titulo del dashboardHeader en admin

--- src/Components/Layout/DashboardHeader/DashboardHeader.jsx
+++ src/Components/Layout/DashboardHeader/DashboardHeader.jsx
@@ -8,7 +8,7 @@ export const DashboardHeader = ({space1,space2,space3,space4,space5,space6,space
       <p className={space2}>{header2}</p>
       <p className={space3}>{header3}</p>
       <p className={space4}>{header4}</p>
-      <p className={space5 + propsReport}>{header5}</p>
+      <p className={space5 + propsReport + ' text_center'}>{header5}</p>
       <p className={space6}>{header6}</p>
       <p className={space7}>{header7}</p>
     </div>
