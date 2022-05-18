e10e3be2e6dc682dbe2a0a292585d48b21f34bbf
Mari293 <maricruzg020903@gmail.com>
2022-05-18 17:52:30 -0500

    mariana_dev - Se valida la seleccion de un checkbox

--- src/Components/Ui/CheckBoxFilterAdmin/CheckBoxFilterAdmin.jsx
+++ src/Components/Ui/CheckBoxFilterAdmin/CheckBoxFilterAdmin.jsx
@@ -18,16 +18,24 @@ export const CheckBoxFilterAdmin = ({designCheckBoxAdmin, id}) => {
     // console.log(e);
     // })
     const validateChecked=(e)=>{
-        if(e.target.checked ){
-            if(e.target.id != 1){   
-            }
-        }
+      if(e.target.checked && e.target.id==1){
+          
+      }
+      else if(e.target.checked && e.target.id==2){
+        return "Se selecciono el 2"
+      }
+      else if(e.target.checked && e.target.id==3){
+        return "Se selecciono el 3"
+      }
+      else if(e.target.checked && e.target.id==4){
+        return "Se selecciono el 4"
+      }
     }
 
   return (
     <div className='text_center fieldSize8' >
       <label className='position_flex_center'>
-          <input type="checkbox" className='cb_confirm_changes' id={id} onClick={(e)=>validateChecked(e)}/>
+          <input type="checkbox" className='cb_confirm_changes' id={id} onClick={(e)=>console.log(validateChecked(e))}/>
           <span className={designCheckBoxAdmin}></span>
       </label>
     </div>  )
