37034c2b79aaf04077d1fd13a555a7dbf3bda3a2
mari293 <maricruzg020903@gmail.com>
2022-05-22 01:02:15 -0500

    mariana_dev - Se hace validación para que se permita deseleccionar el radio button en filtros

--- src/Components/Ui/FilterUserAdmin/FilterUserAdmin.jsx
+++ src/Components/Ui/FilterUserAdmin/FilterUserAdmin.jsx
@@ -11,35 +11,41 @@ export const FilterUserAdmin = ({setIdFilter}) => {
     setIdFilter(e.target.value);
   }
   
+  const unSelect = (e) => {
+    return e.target.checked = false
+  }
+
+
+
   return (
     <div className='content_filter'>
         <Button className="button btn_search_filter_admin_users" value={"Filtro"} onClick={()=>{setIsOpen(!isOpen)}}/>
       <PopUp isOpen={isOpen}> 
         <div className='overlay overlay_options' onClick={()=>{setIsOpen(!isOpen)}}></div>
-        <div className='content_options filter_options' onChange={changeValueSelected}>
+        <div className='content_options filter_options' onChange={changeValueSelected}> 
           <div className='typ_report'>
             <label>
               Tipos de suspensión
             </label>
-            <input type="radio" value="1" name ="opcion1"/>
+            <input type="radio" value="1" name ="filters" onDoubleClick={(e)=>unSelect(e)}/>
           </div>
           <div className='typ_report'>
             <label>
               Reportes
             </label>
-            <input type="radio" value="2" name ="opcion1"/>
+            <input type="radio" value="2" name ="filters" onClick={(e)=>unSelect(e)}/>
           </div>
           <div className='typ_report'>
             <label>
               Correo
             </label>
-            <input type="radio" value="3" name ="opcion1"/>
+            <input type="radio" value="3" name ="filters" onClick={(e)=>unSelect(e)}/>
           </div>
           <div className='typ_report'>
             <label>
               Nombres y Apellidos
             </label>
-            <input type="radio" value="4" name ="opcion1"/>
+            <input type="radio" value="4" name ="filters" onClick={(e)=>unSelect(e)}/>
           </div>
         </div>
       </PopUp>
