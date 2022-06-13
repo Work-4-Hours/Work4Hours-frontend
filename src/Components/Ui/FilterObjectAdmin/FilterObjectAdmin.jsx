6634d252557e85df479ce6d6f6a7fc315279d73d
MajoS7 <majos2764@gmail.com>
2022-06-13 02:21:25 -0500

    majo_dev -Inicio de diseño responsivo de admin

--- src/Components/Ui/FilterObjectAdmin/FilterObjectAdmin.jsx
+++ src/Components/Ui/FilterObjectAdmin/FilterObjectAdmin.jsx
@@ -3,13 +3,15 @@ import React, {useState} from 'react'
 import { Button } from '../Button/Button';
 import { OptionFilterUserAdmin } from '../OptionFilterUserAdmin/OptionFilterUserAdmin';
 import './FilterObjectAdmin.css'
+import { BsThreeDotsVertical } from 'react-icons/bs';
 
 export const FilterObjectAdmin = ({dataFilter}) => {
     const{changeFilteringOptionId,unSelect,data}=dataFilter;
     const [isOpen, setIsOpen] = useState(false);
   return (
     <div className='content_filter'>
-        <Button className="button btn_search_filter_admin_users" value={"Filtro"} onClick={()=>{setIsOpen(!isOpen)}}/>
+        <BsThreeDotsVertical className='icon_filter_admin' onClick={()=>{setIsOpen(!isOpen)}}/>
+        <Button className="button btn_search_filter_admin_users remove_button" value={"Filtro"} onClick={()=>{setIsOpen(!isOpen)}}/>
       <PopUp isOpen={isOpen}> 
         <div className='overlay overlay_options' onClick={()=>{setIsOpen(!isOpen)}}></div>
         <div className='content_options filter_options' onChange={changeFilteringOptionId}>
