aee9ce09f4a2099a47c909f0fbd639e1550477a0
MajoS7 <majos2764@gmail.com>
2022-06-12 22:39:11 -0500

    majo_dev -Menu desplegable de admin

--- src/Components/Layout/MenuAdmin/MenuAdmin.jsx
+++ src/Components/Layout/MenuAdmin/MenuAdmin.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './MenuAdmin.css';
 
-import { Button } from 'Components/Ui/Button/Button';
-import { TitleAdmin } from 'Components/Ui/TitleAdmin/TitleAdmin';
-
-
+import {VerticalAdminMenu} from '../VerticalAdminMenu/VerticalAdminMenu';
+import { DropDownAdminMenu } from '../DropDownAdminMenu/DropDownAdminMenu';
 
 export const MenuAdmin = ({dataMenuAdmin}) => {
-  const {logoutAdmin, nameAdmin, buttonActivated, buttonDeactivated}=dataMenuAdmin;
+  const{logoutAdmin}=dataMenuAdmin;
+  
   const navigate = useNavigate()
   
   const logoutApp = () => {
@@ -17,15 +16,9 @@ export const MenuAdmin = ({dataMenuAdmin}) => {
   }
 
   return (
-    <div className='menu_admin'>
-      <div>
-        <TitleAdmin typeAdmin={nameAdmin}/>
-        <div className='btns_menu_admin'>
-          <Link to="/AdminUsers" className='text_decoration_none'><Button className={"button btn_with_admin "+ buttonActivated} value={"Usuarios"}/></Link>
-          <Link to="/AdminServices" className='text_decoration_none'><Button className={"button btn_with_admin "+ buttonDeactivated} value={"Servicios"}/></Link>
-        </div>
-      </div>
-      <Button className="button btn_change_color_gray btn_with_admin" value={"Cerrar Sesión"} onClick={logoutApp}/>
-    </div> 
+    <>
+    <VerticalAdminMenu dataMenuAdmin={dataMenuAdmin} logoutApp={logoutApp}/>
+    <DropDownAdminMenu dataMenuAdmin={dataMenuAdmin} logoutApp={logoutApp}/>
+    </>
   )
 }
